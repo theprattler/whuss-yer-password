4b035a6dc75dbba1e204bc2e7f51bd90901f3d2d
The Prattler <jmichaelsmith@juno.com>
2021-10-28 11:37:10 -0400

    removed functions for char steps and added ability to put selected chars in new string

--- Develop/script.js
+++ Develop/script.js
@@ -1,101 +1,67 @@
 // Assignment code here
+var string = "";
 var charLower = "abcdefghijklmnopqrstuvwxyz";
 var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charNumber = "0123456789";
 var charSpecial = "!@#$%^&*-_=+";
 
+
 // window alert welcome
 var welcome = window.alert("Welcome to my Password Generator! Let me ask you a few questions to serve your needs.");
 
 // user chooses password length
-var passwordLength = function() {
-  // ask user to choose between 8 and 128 characters
-  var promptPassLength = "";
-  while (promptPassLength === "" || promptPassLength === null) {
-    promptPassLength = prompt("How many characters long would you like your password to be? Please choose a number between 8 and 128.");
-  }
-  console.log("Your password length is " + promptPassLength + " characters long.");
-  return promptPassLength;
-
-  /* conditional recursive function call
-  if (promptPassLength < 8 || promptPassLength > 128) {
-    window.alert("Please choose a number between 8 and 128.");
-    return passwordLength();
-  }*/
-};
+promptPassLength = prompt("How many characters long would you like your password to be? Please choose a number between 8 and 128.");
+console.log("Your password length is " + promptPassLength + " characters long.");
 
 // user chooses whether to include lower case alphabet
-var lowerCaseOption = function() {
-  var confirmPassLower = confirm("Would you like to include lowercase letters?");
+var confirmPassLower = confirm("Would you like to include lowercase letters?");
 
   // if yes (true), store answer
   if (confirmPassLower) {
     console.log("You chose yes for lowercase letters.");
+    console.log(confirmPassLower);
+    
   } else {
     console.log("You chose no for lowercase letters.");
-  }
-
-  /* conditional recursive fun
-  if () {
-    window.alert("You need to provide a valid answer. Please type YES or NO.");
-    return lowerCaseOption();
-  }*/
-
-  return confirmPassLower;
-};
+    console.log(confirmPassLower);
+    
+  };
 
 // user chooses whether to include upper case alphabet
-var upperCaseOption = function() {
-  var confirmPassUpper = confirm("Would you like to include uppercase letters?");
+var confirmPassUpper = confirm("Would you like to include uppercase letters?");
   
   // if yes (true), store answer
   if (confirmPassUpper) {
     console.log("You chose yes for uppercase letters.");
+    
   } else {
-    console.log("You chose no for uppercase letters.")
-  }
-  /*if (promptPassUpper === "" || promptPassUpper === null) {
-    window.alert("You need to provide a valid answer. Please type YES or NO.");
-    return upperCaseOption();
-  }*/
-
-  return confirmPassUpper;
-};
-
+    console.log("You chose no for uppercase letters.");
+    
+  };
+  
 // user chooses whether to include numericals
-var numberOption = function() {
-  var confirmPassNum = confirm("Would you like to include numbers?");
+var confirmPassNum = confirm("Would you like to include numbers?");
   
   // if yes (true), store answer
   if (confirmPassNum) {
     console.log("You chose yes for numbers.");
+    
   } else {
     console.log("You chose no for numbers.");
-  }
+    
+  };
   
-    /*window.alert("You need to provide a valid answer. Please type YES or NO.");
-    return numberOption();
-  }*/
-
-  return confirmPassNum;
-};
-
 // user chooses whether to include special characters
-var specialOption = function() {
-  var confirmPassSpec = confirm("Would you like to include special characters?");
+var confirmPassSpec = confirm("Would you like to include special characters?");
   
   // if yes (true), store answer
   if (confirmPassSpec) {
     console.log("You chose yes for special characters.");
+    
   } else {
     console.log("You chose no for special characters.");
-  }
-    /*("You need to provide a valid answer. Please type YES or NO.");
-    return specialOption();
-  }*/
-
-  return confirmPassSpec;
-};
+    
+  };
 
 // password info array
 var passwordInfo = [
@@ -104,9 +70,28 @@ var passwordInfo = [
   }
 ];
 
+// add characters to string if selected
+var characterPool = string;
+  if (confirmPassLower) {
+    characterPool += charLower;
+  }
+  if (confirmPassUpper) {
+    characterPool += charUpper;
+  }
+  if (confirmPassNum) {
+    characterPool += charNumber;
+  }
+  if (confirmPassSpec) {
+    characterPool += charSpecial;
+  }
+  
+console.log(characterPool);
+
 
 var generatePassword = function() {
-
+  for (var i = 0; i < promptPassLength; i++) {
+    
+  }
 };
 
 // Get references to the #generate element
@@ -121,11 +106,7 @@ function writePassword() {
 
 };
 
-passwordLength();
-lowerCaseOption();
-upperCaseOption();
-numberOption();
-specialOption();
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
